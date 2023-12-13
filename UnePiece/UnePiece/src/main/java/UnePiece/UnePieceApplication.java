7e37eea48e7461b0c2633e834a7b3a5f449f4c4e
LeZoyberg <pierre.langlade@outlook.fr>
2023-12-13 17:05:15 +0100

    Creation projet UnePiece

---
+++ UnePiece/UnePiece/src/main/java/UnePiece/UnePieceApplication.java
@@ -0,0 +1,13 @@
+package UnePiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnePieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnePieceApplication.class, args);
+	}
+
+}
---
+++ UnePiece/UnePiece/src/test/java/UnePiece/UnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package UnePiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnePieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
