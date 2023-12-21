c9e179d8502791100de4ed45bb89ad9c88b0fb9f
Noh4ns3n <rigolot.vincent@gmail.com>
2023-12-21 16:38:01 +0100

    ajout environment.ts, partie.service.ts, import HttpClientModule

--- UnePieceAngular/src/app/app.module.ts
+++ UnePieceAngular/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
