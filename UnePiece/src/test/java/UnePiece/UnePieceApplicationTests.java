7db3e042e8826330cc5ba4a5f23afe73e537cb9f
Noh4ns3n <rigolot.vincent@gmail.com>
2023-12-21 13:48:27 +0100

    modif initBdd pour que les actions aient une partie associée

--- UnePiece/src/test/java/UnePiece/UnePieceApplicationTests.java
+++ UnePiece/src/test/java/UnePiece/UnePieceApplicationTests.java
@@ -69,6 +69,7 @@ class UnePieceApplicationTests {
 	Bateau bateau2 = new Bateau("Thousand Sunny",15,15,15,false);
 	
 	Navire navire1 = new Navire(bateau1.getRobustesse(), bateau1);
+	Navire navire2 = new Navire(bateau2.getRobustesse(), bateau2);
 	
 	Ile ile1 = new Ile("Fuschia Village", true, true, true, 10, 1, Mer.EastBlue);
 	Ile ile2 = new Ile("Shells Town", false, false, true, 8, 2, Mer.EastBlue);
@@ -78,14 +79,19 @@ class UnePieceApplicationTests {
 	Evenement event2 = new Evenement(15,15,10,Odyssee.Bataille);
 	Evenement event3 = new Evenement(0,0,50,Odyssee.Tresor);
 	
-	Joueur joueur1 = new Joueur("player1", "password1");
+	Joueur joueur1 = new Joueur("player1", "password");
+	Joueur joueur2 = new Joueur("player2", "password");
 	
 	Action action1 = new Action(true, event1.getDegatNavire(), event1.getDegatMembre(), event1.getTresor(), event1);
 	Action action2 = new Action(false, event2.getDegatNavire(), event2.getDegatMembre(), event2.getTresor(), event2);
-	List<Action> actions = new ArrayList<Action>();
-	Collections.addAll(actions, action1, action2);
+	Action action3 = new Action(false, event3.getDegatNavire(), event3.getDegatMembre(), event3.getTresor(), event3);
+	List<Action> actionsPartie1 = new ArrayList<Action>();
+	Collections.addAll(actionsPartie1, action1, action2);
+	List<Action> actionsPartie2 = new ArrayList<Action>();
+	Collections.addAll(actionsPartie2, action3);
 	
-	Partie partie1 = new Partie(LocalDate.now(), false, 100, 10, membres, ile1, navire1, joueur1, actions);
+	Partie partie1 = new Partie(LocalDate.now(), false, 100, 10, membres, ile1, navire1, joueur1, actionsPartie1);
+	Partie partie2 = new Partie(LocalDate.now(), false, 100, 10, membres, ile2, navire2, joueur2, actionsPartie2);
 	
 	
 	daoPirate.save(pirate1);
@@ -101,6 +107,7 @@ class UnePieceApplicationTests {
 	daoBateau.save(bateau2);
 	
 	daoNavire.save(navire1);
+	daoNavire.save(navire2);
 	
 	daoIle.save(ile1);
 	daoIle.save(ile2);
@@ -111,10 +118,17 @@ class UnePieceApplicationTests {
 	daoEvenement.save(event3);
 	
 	daoCompte.save(joueur1);
+	daoCompte.save(joueur2);
+	
+	partie1 = daoPartie.save(partie1);
+	partie2 = daoPartie.save(partie2);
+	
+	action1.setPartie(partie1);
+	action2.setPartie(partie1);
+	action3.setPartie(partie2);
 	
 	daoAction.save(action1);
 	daoAction.save(action2);
-	
-	daoPartie.save(partie1);
+	daoAction.save(action3);
 	}
 }
