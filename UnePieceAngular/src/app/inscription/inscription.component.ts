82952dd8de4b8ece098314fac9673e2aa87fcfac
RaphaelMartin29 <raphaelmartin@hotmail.fr>
2023-12-22 12:40:56 +0100

    Inscription is working
    . Add function mapped in CompteRestController for 'inscription'
    . Routing in front-end for inscription
    . Hide/Show 'Connexion' & 'Inscription' & 'Logout' if isLogged or not
    
    . Don't take into account the validator for the moment

--- UnePieceAngular/src/app/inscription/inscription.component.ts
+++ UnePieceAngular/src/app/inscription/inscription.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { confirmEqualValidator } from '../confirm-equal-validator.validator';
 
 @Component({
   selector: 'inscription',
@@ -14,19 +15,31 @@ export class InscriptionComponent implements OnInit {
 
   usernameCtrl!: FormControl;
   passwordCtrl!: FormControl;
+  //validatePasswordCtrl!: FormControl;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
 
   }
 
   ngOnInit(): void {
     this.usernameCtrl = this.formBuilder.control("", Validators.required);
     this.passwordCtrl = this.formBuilder.control("", [Validators.required, Validators.minLength(5)]);
+    //this.validatePasswordCtrl = this.formBuilder.control("", [Validators.required]);
 
     this.inscriptionForm = this.formBuilder.group({
       username: this.usernameCtrl,
-      password: this.passwordCtrl
-    });
+      password: this.passwordCtrl,
+      //validatePassword: this.validatePasswordCtrl
+      }
+      /*,
+      {
+        validator: [confirmEqualValidator('password', 'validatePassword')]
+      }*/
+    );
+  }
+
+  inscription() {
+    this.authService.inscription(this.usernameCtrl.value, this.passwordCtrl.value);
   }
 
 }
