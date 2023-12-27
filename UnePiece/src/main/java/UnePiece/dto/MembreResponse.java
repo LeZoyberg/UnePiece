1d898e7f9398005df52a84fb6139e79a3795628e
Augustin <augustinvautier@orange.fr>
2023-12-27 19:40:47 +0100

    Tentative autoincrement nbMembre et affichage membres dans les stat equipage

---
+++ UnePiece/src/main/java/UnePiece/dto/MembreResponse.java
@@ -0,0 +1,29 @@
+package UnePiece.dto;
+
+public class MembreResponse {
+	
+	private String nom;
+	private int pv;
+	private int power;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getPv() {
+		return pv;
+	}
+	public void setPv(int pv) {
+		this.pv = pv;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+	
+}
