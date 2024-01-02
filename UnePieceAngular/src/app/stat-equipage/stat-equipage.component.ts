28a2f4b89d38cb52c00b3ad25594dc9d1fb0b514
Augustin <augustinvautier@orange.fr>
2024-01-02 17:38:54 +0100

    Bouton jours suivant/envoie ile fait mais marche pas
    plus nettoyage stat equipage

--- UnePieceAngular/src/app/stat-equipage/stat-equipage.component.ts
+++ UnePieceAngular/src/app/stat-equipage/stat-equipage.component.ts
@@ -22,29 +22,6 @@ color: string = "#2C75FF";
 visible: boolean = false;
 membres!: Membre[]; 
 
-  /*constructor(private membreService: MembreService, private navireService: NavireService) {
-    this.load();
-  }
-
-  load() {
-    // aller choper liste des membres de la partie grâce au partie service, genre : this.partieService.getPartie().getMembres
-    console.log("avant");
-    this.membreService.findAll().subscribe(resp => {
-      console.log("entrer");
-      this.membres = resp;
-      console.log(this.membres); 
-      this.nbMembre=this.membres.length;
-      this.vie=this.membres[0].pv;
-      this.tresor=this.membres[0].partie?.tresor;
-      for (let membre of this.membres){
-        if (membre.power){this.force+=membre.power;};
-      }
-    });
-    this.navireService.findById(1).subscribe(resp => {
-      this.robustesse = resp.robustesse;
-    });
-  }*/
-
   constructor (private partieService: PartieService){
     let user = localStorage.getItem('user')
     if(user){
@@ -54,23 +31,16 @@ membres!: Membre[];
   }
 
   load(){
-    console.log("joueur : " + this.joueur.id);
     this.partieService.findByIdJoueurWithMembres(this.joueur.id).subscribe(resp => {
       this.membres=resp.membres;
-      console.log("members : " +  JSON.stringify(this.membres))
       this.nbMembre=this.membres.length;
-      console.log("nbMembre : " + this.nbMembre)
       this.vie=this.membres[0].pv;
-      console.log("vie : " + this.vie)
       this.tresor=resp.tresor;
-      console.log("tresor : " + this.tresor)
       for (let membre of this.membres){
         if (membre.power){this.force+=membre.power;};
       }
-      console.log("power : " + this.force);
-      console.log(`navire : ${resp.navire}`)
+      //console.log(`navire : ${resp.navire}`)
       this.robustesse=resp.navire?.robustesse;
-      console.log("robustesse : " + this.robustesse);
     });
   }
 
--- UnePieceAngular/src/app/trajet/trajet.component.ts
+++ UnePieceAngular/src/app/trajet/trajet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PartieService } from '../partie.service';
+import { Joueur, Partie } from '../model';
 
 @Component({
   selector: 'app-trajet',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./trajet.component.css']
 })
 export class TrajetComponent {
+nbJours: number = 3;
+ile?: number;
+joueur!: Joueur;
+
+  constructor(
+    private router: Router,
+    private partieService: PartieService,
+  ) {
+    let user = localStorage.getItem('user')
+    if(user){
+      this.joueur = JSON.parse(user) as Joueur;
+      this.partieService.findByIdJoueur(this.joueur.id).subscribe(resp => {
+        console.log("ile id"+resp.ile?.id)
+        this.ile = resp.ile?.id as number;
+      });
+    };
+  }
+
+Suite(){
+  if (!(this.nbJours==0)){
+    this.nbJours--;
+  }
+  else{
+    this.nbJours=3;
+    console.log(`this ile id ${this.ile}`);
+    this.router.navigate(['/ile/'+this.ile]);
+  }
+}
 
 }
